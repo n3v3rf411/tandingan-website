68aff826f6a113d230f08badfbb46080d330f81e
Willie Loyd Tandingan <tandingan.wlb@gmail.com>
2023-01-11 02:14:28 +0900

    Update config page_url

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://neverfall.notion.site/Website-ni-Tandingan-d17d3dc120c04b869f164421bc8420e3',
         pages: [
             // {
             //     page_id: '<page_id>',
